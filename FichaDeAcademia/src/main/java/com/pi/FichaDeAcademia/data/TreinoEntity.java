d3b718fec6643b86b64a712a4fa725fba0ce2e12
Alexandra Matte <alsmatte.art@gmail.com>
2024-11-19 11:56:36 -0300

    Criação das classes Entity e Repository

---
+++ FichaDeAcademia/src/main/java/com/pi/FichaDeAcademia/data/TreinoEntity.java
@@ -0,0 +1,41 @@
+package com.pi.FichaDeAcademia.data;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Set;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Treino")
+public class TreinoEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    //@NotNull(message="Informe o nome do treino")
+    //@NotBlank(message="Informe o nome do treino")
+    private String nome;
+    
+    //@NotNull(message="Informe o tipo de treino (ex: braço, perna, etc)")
+    //@NotBlank(message="Informe o tipo de treino (ex: braço, perna, etc)")
+    private String tipo;
+    
+    //@NotBlank(message="Informe quantos dias na semana")
+    private int frequencia;
+    
+    private String diasSemana;
+    
+    @OneToMany(mappedBy = "treino", cascade = CascadeType.ALL)
+    private Set<ExercicioEntity> exercicios;
+    
+}
